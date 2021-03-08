b0a2e257eee6a3aa34434d0529f76f5a8c2e1110
thaitam <phithaitam@gmail.com>
2021-03-08 14:35:39 +0700

    chuyển json string thành data

--- src/json.js
+++ src/json.js
@@ -2,4 +2,4 @@ const jsonString = '[{"name":"join","score":51},{"name":"anh","score":23}]';
 const data = JSON.parse(jsonString,(key,value)=>{
    return key==="name"?value.toUpperCase():value
 });
-console.log(data[1]);
+console.log(data[0]);
