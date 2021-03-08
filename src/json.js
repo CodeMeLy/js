26f54c6c8e61509475bf5fadb85f6df54fae9c11
thaitam <phithaitam@gmail.com>
2021-03-08 14:42:10 +0700

    chuyển json thành date

--- src/json.js
+++ src/json.js
@@ -3,3 +3,9 @@ const data = JSON.parse(jsonString,(key,value)=>{
    return key==="name"?value.toUpperCase():value
 });
 console.log(data[0]);
+// chuyển sang ngày tháng
+const jsonString2 = '{"date":"2016-01-04T23:00:00.000Z"}';
+const date = JSON.parse(jsonString2,(key,value)=>{
+    return key === "date"?(new Date(value)):value;
+})
+console.log(date);
